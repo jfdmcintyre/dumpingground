d255e29bb7f4a00557a7e3a011ee66cbef3c6498
jfdmcintyre <146741478+jfdmcintyre@users.noreply.github.com>
2024-07-26 22:15:04 -0300

    modified:   messin.ps1

--- messin.ps1
+++ messin.ps1
@@ -1,10 +1,12 @@
+# Add a progress indicator Label
 $progressLabel = New-Object System.Windows.Forms.Label
 $progressLabel.Size = New-Object System.Drawing.Size(30, 30)
-$progressLabel.Location = New-Object System.Drawing.Point(300, 320)
+$progressLabel.Location = New-Object System.Drawing.Point(530, 440)
 $progressLabel.Font = New-Object System.Drawing.Font("Consolas", 16)
 $progressLabel.TextAlign = [System.Drawing.ContentAlignment]::MiddleCenter
+$progressLabel.ForeColor = [System.Drawing.Color]::White
 $progressLabel.Visible = $false
-$wslBackupForm.Controls.Add($progressLabel)
+$wslImportForm.Controls.Add($progressLabel)
 
 # Create a timer for updating the progress indicator
 $progressTimer = New-Object System.Windows.Forms.Timer
